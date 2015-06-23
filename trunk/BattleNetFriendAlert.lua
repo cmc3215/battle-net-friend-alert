687304b3e7475d9a5575dc3761aa95808c06b6a3
cmc3215 <cmc3215@ffe1e499-a5c3-49a1-b21f-62ff599db03a>
2015-06-23 08:29:25 +0000

    Patch 6.2 updates

--- trunk/BattleNetFriendAlert.lua
+++ trunk/BattleNetFriendAlert.lua
@@ -1,10 +1,10 @@
 --------------------------------------------------------------------------------------------------------------------------------------------
--- Variables
+-- Initialize Variables
 --------------------------------------------------------------------------------------------------------------------------------------------
-local BNFA_INTERVAL = 3;
-local BNFA_TOTAL_ELAPSED = 0;
-local BNFA_FRIENDS = {};
-local BNFA_ICON = {
+local NS = select( 2, ... );
+NS.interval = 3; -- Seconds between ScanFriends()
+NS.friends = {};
+NS.icons = {
 	["Friend"] = "|TInterface\\FriendsFrame\\UI-Toast-FriendOnlineIcon:16:16:0:0:32:32:2:30:2:30|t",
 	["App"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-Battlenet:14|t",
 	["WoW"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-WoW:14|t",
@@ -13,58 +13,39 @@ local BNFA_ICON = {
 	["Hero"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-HotS:14|t",
 	["S2"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-SC2:14|t",
 };
-local L = BNFA_LOCALIZATION;
+local L = NS.localization;
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- Functions
 --------------------------------------------------------------------------------------------------------------------------------------------
-function BNFA_ScanFriendsTimer( self, elapsed )
-	BNFA_TOTAL_ELAPSED = BNFA_TOTAL_ELAPSED + elapsed;
-	if BNFA_TOTAL_ELAPSED >= BNFA_INTERVAL then
-		self:SetScript( "OnUpdate", nil );
-		BNFA_ScanFriends();
-		BNFA_TOTAL_ELAPSED = 0;
-		self:SetScript( "OnUpdate", BNFA_ScanFriendsTimer );
-	end
-end
-
---
-function BNFA_HBNPlayer( presenceName, presenceID )
+NS.BNPlayerLink = function( presenceName, presenceID )
 	return string.format( "|HBNplayer:%s:%s|h[%s]|h", presenceName, presenceID, presenceName );
 end
 
 --
-function BNFA_ScanFriends()
+NS.ScanFriends = function()
 	if BNConnected() then
-		local total, online = BNGetNumFriends();
-		--
-		for index = 1, total do
-			local presenceID,presenceName,_,_,characterName,_,game,_,_,_,_,_,_,_,_,_ = BNGetFriendInfo( index ); -- presenceID, presenceName, battleTag, isBattleTagPresence, characterName, toonID, game, isOnline, lastOnline, isAFK, isDND, messageText, noteText, isRIDFriend, broadcastTime, canSoR = BNGetFriendInfo( friendIndex )
-			-- Make sure friend is online now and was online during last scan
-			if game and BNFA_FRIENDS[presenceID] and BNFA_FRIENDS[presenceID]["game"] then
-				-- Alert, friend has switched games (App considered a game)
-				if game ~= BNFA_FRIENDS[presenceID]["game"] then
+		for index = 1, BNGetNumFriends() do
+			local presenceID,presenceName,_,_,characterName,_,game = BNGetFriendInfo( index );
+			if game and NS.friends[presenceID] and NS.friends[presenceID]["game"] then -- Make sure friend is online now and was online during last scan
+				if game ~= NS.friends[presenceID]["game"] then -- Alert, friend has switched games (App considered a game)
 					if game == "App" then
-						print( BATTLENET_FONT_COLOR_CODE .. BNFA_ICON["Friend"] .. string.format( L["%s stopped playing (%sIn Battle.net)."], BNFA_HBNPlayer( presenceName, presenceID ), BNFA_ICON[game] ) .. "|r" );
+						print( BATTLENET_FONT_COLOR_CODE .. NS.icons["Friend"] .. string.format( L["%s stopped playing (%sIn Battle.net)."], NS.BNPlayerLink( presenceName, presenceID ), NS.icons[game] ) .. FONT_COLOR_CODE_CLOSE );
 					else
-						print( BATTLENET_FONT_COLOR_CODE .. BNFA_ICON["Friend"] .. string.format( L["%s is now playing (%s%s)."], BNFA_HBNPlayer( presenceName, presenceID ), BNFA_ICON[game], characterName ) .. "|r" );
+						print( BATTLENET_FONT_COLOR_CODE .. NS.icons["Friend"] .. string.format( L["%s is now playing (%s%s)."], NS.BNPlayerLink( presenceName, presenceID ), NS.icons[game], characterName ) .. FONT_COLOR_CODE_CLOSE );
 						PlaySound( "UI_BnetToast" );
 					end
 				end
 			end
 			-- Record latest friend info
-			BNFA_FRIENDS[presenceID] = { ["game"] = game };
+			NS.friends[presenceID] = { ["game"] = game };
 		end
 	end
+	-- Scan again in interval seconds
+	C_Timer.After( NS.interval, NS.ScanFriends );
 end
 --------------------------------------------------------------------------------------------------------------------------------------------
--- Events
+-- EventsFrame
 --------------------------------------------------------------------------------------------------------------------------------------------
-local f = CreateFrame( "Frame", "BNFA_EventsFrame", UIParent );
-f:SetScript( "OnEvent", function ( self, event, ... )
-	if event == "PLAYER_LOGIN" then
-		self:UnregisterEvent( "PLAYER_LOGIN" );
-		self:SetScript( "OnEvent", nil );
-		self:SetScript( "OnUpdate", BNFA_ScanFriendsTimer );
-	end
-end );
+local f = CreateFrame( "Frame", "BNFAEventsFrame", UIParent );
+f:SetScript( "OnEvent", function () C_Timer.After( NS.interval, NS.ScanFriends ); end ); -- Scan interval seconds after PLAYER_LOGIN
 f:RegisterEvent( "PLAYER_LOGIN" );
