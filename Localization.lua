d32e0e6ad342d9914d3a09fba41cd554d50b7805
cmc3215 <cmc3215@ffe1e499-a5c3-49a1-b21f-62ff599db03a>
2016-03-28 18:08:26 +0000

    New file changes for tag v1.9

--- Localization.lua
+++ Localization.lua
@@ -21,20 +21,29 @@ if GetLocale() == "enUS" or GetLocale() == "enGB" then
 -- L["Overwatch"] = ""
 -- deDE
 elseif GetLocale() == "deDE" then
+--@localization(locale="deDE", format="lua_additive_table")@
 -- esES
 elseif GetLocale() == "esES" then
+--@localization(locale="esES", format="lua_additive_table")@
 -- frFR
 elseif GetLocale() == "frFR" then
+--@localization(locale="frFR", format="lua_additive_table")@
 -- itIT
 elseif GetLocale() == "itIT" then
+--@localization(locale="itIT", format="lua_additive_table")@
 -- koKR
 elseif GetLocale() == "koKR" then
+--@localization(locale="koKR", format="lua_additive_table")@
 -- ptBR
 elseif GetLocale() == "ptBR" then
+--@localization(locale="ptBR", format="lua_additive_table")@
 -- ruRU
 elseif GetLocale() == "ruRU" then
+--@localization(locale="ruRU", format="lua_additive_table")@
 -- zhCN
 elseif GetLocale() == "zhCN" then
+--@localization(locale="zhCN", format="lua_additive_table")@
 -- zhTW
 elseif GetLocale() == "zhTW" then
+--@localization(locale="zhTW", format="lua_additive_table")@
 end
